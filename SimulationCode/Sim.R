51a585f199889cc9186cb1919a100f5a4c74406e
Jessica Yu <jeyu22@amherst.edu>
2022-03-23 18:19:33 -0400

    updates to chapter 2

--- SimulationCode/Sim.R
+++ SimulationCode/Sim.R
@@ -4,7 +4,8 @@ library(lmerTest)
 library(SimMultiCorrData)
 library(purrr)
 library(broom)
-
+library(broom.mixed)
+library(SimDesign) # to quiet rcorrvar 
 
 ## Function that returns p values from Kenward-Roger and Satterthwaite from linear mixed model
 sim_mixedeffects <- function(dist, parameters, random_slopes = TRUE,
@@ -18,26 +19,31 @@ sim_mixedeffects <- function(dist, parameters, random_slopes = TRUE,
 
   if (random_slopes == TRUE) {
     r_effects <- rcorrvar(
-      n = n.indiv, k_cont = 2, method = "Fleishman", means = c(m[1], m[1]), vars = c(m[2]^2, m[2]^2), skews = c(m[3], m[3]),
+      n = n.indiv, k_cont = 2, method = "Fleishman", means = c(m[1], m[1]), vars = c(m[2]^2, (m[2]^2)/18), skews = c(m[3], m[3]),
       skurts = c(m[4], m[4]),
       errorloop = TRUE, rho = corr1, seed = sample(1000:9999, 1)
     )
 
     r_effects1 <- r_effects$continuous_variables$V1 - m[1]
     r_effects2 <- r_effects$continuous_variables$V2 - m[1]
-  } else {
+    
+    errors <- rnorm(n.indiv * n.measurements, 0, m[2]*sqrt(.168))
+  } else { # random intercept 
     r_effects1 <- nonnormvar1("Fleishman",
       means = m[1], vars = m[2]^2, skews = m[3],
       skurts = m[4], n = n.indiv, seed = sample(1000:9999, 1)
     )$continuous_variable$V1
-
+    
+    r_effects1 <- r_effects1 - m[1]
     r_effects2 <- rep(0, n.indiv)
+    errors <- rnorm(n.indiv * n.measurements , 0, m[2]*sqrt(.4))
   }
 
-  data <- expand.grid(indiv = as.factor(1:n.indiv), time = 1:n.measurements)
-  matrix_fixedeffects <- model.matrix(~time, data) # model matrix for fixed effects
+  data <- expand.grid(time = 1:n.measurements, indiv = as.factor(1:n.indiv))
+  data$treatment <- rep(0:1, each = (n.indiv*n.measurements)/2)
+  matrix_fixedeffects <- model.matrix(~time + treatment, data) # model matrix for fixed effects
 
-  betas <- c(3.1, 0) # fixed effects coefficient vector
+  betas <- c(3.1, 0,0) # fixed effects coefficient vector
 
   # model matrix for random intercepts
   matrix_intercept <- model.matrix(~ 0 + indiv, data) 
@@ -45,58 +51,39 @@ sim_mixedeffects <- function(dist, parameters, random_slopes = TRUE,
   # model matrix for random slopes
   matrix_slope <- model.matrix(~ 0 + indiv, data) * data$time 
 
-  
-  errors <- rnorm(nrow(data), 0, .2)
 
-
-  data$Y_val <- matrix_fixedeffects %*% betas + 
+  data$Y_val <- (matrix_fixedeffects %*% betas + 
      matrix_intercept %*% r_effects1 + 
-     matrix_slope %*% r_effects2 + errors
+     matrix_slope %*% r_effects2 + errors)[,1]
+  
+  
 
   # construct model
   if (random_slopes == FALSE) {
-    model <- lmerTest::lmer(Y_val ~ time + (1 | indiv), data)
-    var_ranef_df <- data.frame(VarCorr(model))
-    var_ranef <- data.frame(
-      var_rand_intercept = var_ranef_df[1, 4],
-      var_rand_residual = var_ranef_df[2, 4]
-    )
+    model <- lmerTest::lmer(Y_val ~ time + (1 | indiv) + treatment, data)
+ 
+  
   } else {
-    model <- lmerTest::lmer(Y_val ~ time + (time | indiv), data)
-    var_ranef_df <- data.frame(VarCorr(model))
-    var_ranef <- data.frame(
-      var_rand_intercept = var_ranef_df[1, 4],
-      var_rand_slope = var_ranef_df[2, 4],
-      var_rand_residual = var_ranef_df[4, 4]
-    )
+    model <- lmerTest::lmer(Y_val ~ time + (time | indiv) + treatment, data)
   }
 
-
-  a_KR <- tidy(anova(model, ddf = "Kenward-Roger"))
-  a_S <- tidy(anova(model))
-  p <- data.frame(
-    distribution = dist, number_individuals = n.indiv,
-    params = paste0(parameters, collapse = ","),
-    number_measurements = n.measurements,
-    rslope = random_slopes, mean = m[1], sd = m[2], skew = m[3], kurtosis = m[4],
-    fifth = m[5], sixth = m[6]
-  )
-
-
-  colnames(a_KR) <- paste("KR", colnames(a_KR), sep = "_")
-  colnames(a_S) <- paste("S", colnames(a_S), sep = "_")
+  KR <- broom.mixed::tidy(model, ddf = "Kenward-Roger") 
+  colnames(KR) <- paste("KR", colnames(KR), sep = "_")
+  
+  S <- broom.mixed::tidy(model, ddf = "Satterthwaite") %>%
+    rename_with(.cols = everything(), .fn = ~str_c("S_", .x)) %>%
+    add_column(.before = "S_effect",
+               distribution = dist, number_individuals = n.indiv,
+               params = paste0(parameters, collapse = ","),
+               number_measurements = n.measurements,
+               rslope = random_slopes, mean = m[1], sd = m[2], skew = m[3], kurtosis = m[4],
+               fifth = m[5], sixth = m[6])
 
 
-  return(cbind(a_KR, a_S, p, var_ranef))
+
+
+  return(cbind(S,KR))
 }
-
-sim_mixedeffects(
-  dist = "Gaussian", parameters = c(0, 2), random_slopes = TRUE,
-  n.indiv = 10, n.measurements = 4,
-  corr = -.38
-)
-
-
 
 
 ## Plotting the exponential and lognormal distributions
@@ -105,6 +92,7 @@ sim_mixedeffects(
 #### lognormal
 curve(dlnorm(x, meanlog = 0, sdlog = .25), from = 0, to = 5)
 curve(dlnorm(x, meanlog = .5, sdlog = .1), from = 0, to = 5)
+
 curve(dlnorm(x, meanlog = 1, sdlog = .5), from = 0, to = 5)
 curve(dlnorm(x, meanlog = 0, sdlog = .9), from = 0, to = 5)
 
@@ -115,7 +103,7 @@ plot(x, dexp(x, .2),
   ylab = "", lwd = 2, col = "red"
 )
 
-plot(x, dexp(x, 4),
+plot(x, dexp(x, 1),
   type = "l",
   ylab = "", lwd = 2, col = "red"
 )
@@ -129,7 +117,7 @@ plot(x, dexp(x, .9),
 ## Setting up factors for lognormal condition
 dist <- c("Lognormal")
 corr <- c(-.38)
-parameters <- list(c(0, .25), c(.5, .1), c(1, .5), c(0, .9))
+parameters <- list(c(0, .25), c(1, .5))
 random_slopes <- c(TRUE, FALSE)
 n.indiv <- c(10, 18, 26)
 n.measurements <- c(4, 8)
@@ -142,7 +130,7 @@ combos_lnormal <- crossing(dist, parameters, random_slopes,
 ## Setting up factors for exponential condition
 dist <- c("Exponential")
 corr <- c(-.38)
-parameters <- c(4, .2, .9)
+parameters <- c(.2, .9)
 random_slopes <- c(TRUE, FALSE)
 n.indiv <- c(10, 18, 26)
 n.measurements <- c(4, 8)
@@ -152,9 +140,9 @@ combos_exp <- crossing(dist, parameters, random_slopes,
                        n.indiv, n.measurements, corr)
 
 
-numsim <- 10
+numsim <- 20
 
-######
+###### Lognormal 
 set.seed(2021)
 
 simulations_lnormal_1 <- 1:numsim %>%
@@ -198,48 +186,8 @@ simulations_lnormal_5 <- 1:numsim %>%
                      .f = sim_mixedeffects))
 #saveRDS(simulations_lnormal_5, "simulations_lnormal_5.rds")
 
-set.seed(8190)
-simulations_lnormal_6 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_lnormal$dist,combos_lnormal$parameters,
-                          combos_lnormal$random_slopes,combos_lnormal$n.indiv,
-                          combos_lnormal$n.measurements,combos_lnormal$corr),
-                     .f = sim_mixedeffects))
-#saveRDS(simulations_lnormal_6, "simulations_lnormal_6.rds")
 
-
-set.seed(7788)
-simulations_lnormal_7 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_lnormal$dist,combos_lnormal$parameters,
-                          combos_lnormal$random_slopes,combos_lnormal$n.indiv,
-                          combos_lnormal$n.measurements,combos_lnormal$corr),
-                     .f = sim_mixedeffects))
-#saveRDS(simulations_lnormal_7, "simulations_lnormal_7.rds")
-
-set.seed(0317)
-simulations_lnormal_8 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_lnormal$dist,combos_lnormal$parameters,
-                          combos_lnormal$random_slopes,combos_lnormal$n.indiv,
-                          combos_lnormal$n.measurements,combos_lnormal$corr),
-                     .f = sim_mixedeffects))
-#saveRDS(simulations_lnormal_8, "simulations_lnormal_8.rds")
-
-set.seed(0618)
-simulations_lnormal_9 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_lnormal$dist,combos_lnormal$parameters,
-                          combos_lnormal$random_slopes,combos_lnormal$n.indiv,
-                          combos_lnormal$n.measurements,combos_lnormal$corr),
-                     .f = sim_mixedeffects))
-#saveRDS(simulations_lnormal_9, "simulations_lnormal_9.rds")
-
-set.seed(2222)
-simulations_lnormal_10 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_lnormal$dist,combos_lnormal$parameters,
-                          combos_lnormal$random_slopes,combos_lnormal$n.indiv,
-                          combos_lnormal$n.measurements,combos_lnormal$corr),
-                     .f = sim_mixedeffects))
-#saveRDS(simulations_lnormal_10, "simulations_lnormal_10.rds")
-
-## 
+## Normal dist
 
 set.seed(7006)
 simulations_normal_1 <- 1:numsim %>%
@@ -271,32 +219,8 @@ simulations_normal_5 <- 1:numsim %>%
    map_df(~ pmap_dfr(list(combos_normal$dist,combos_normal$parameters,combos_normal$random_slopes,combos_normal$n.indiv,combos_normal$n.measurements,combos_normal$corr),.f = sim_mixedeffects))
 #saveRDS(simulations_normal_5, "simulations_normal_5.rds")
 
-set.seed(3333)
-simulations_normal_6 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_normal$dist,combos_normal$parameters,combos_normal$random_slopes,combos_normal$n.indiv,combos_normal$n.measurements,combos_normal$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_normal_6, "simulations_normal_6.rds")
 
-set.seed(4444)
-simulations_normal_7 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_normal$dist,combos_normal$parameters,combos_normal$random_slopes,combos_normal$n.indiv,combos_normal$n.measurements,combos_normal$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_normal_7, "simulations_normal_7.rds")
-
-set.seed(6666)
-simulations_normal_8 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_normal$dist,combos_normal$parameters,combos_normal$random_slopes,combos_normal$n.indiv,combos_normal$n.measurements,combos_normal$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_normal_8, "simulations_normal_8.rds")
-
-set.seed(7777)
-simulations_normal_9 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_normal$dist,combos_normal$parameters,combos_normal$random_slopes,combos_normal$n.indiv,combos_normal$n.measurements,combos_normal$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_normal_9, "simulations_normal_9.rds")
-
-set.seed(8888)
-simulations_normal_10 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_normal$dist,combos_normal$parameters,combos_normal$random_slopes,combos_normal$n.indiv,combos_normal$n.measurements,combos_normal$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_normal_10, "simulations_normal_10.rds")
-
-#########
+######### Exponential Dist
 
 set.seed(9998)
 simulations_exp_1 <- 1:numsim %>%
@@ -325,29 +249,10 @@ simulations_exp_5 <- 1:numsim %>%
 #saveRDS(simulations_exp_5, "simulations_exp_5.rds")
 
 
-set.seed(9994)
-simulations_exp_6 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_exp$dist,combos_exp$parameters,combos_exp$random_slopes,combos_exp$n.indiv,combos_exp$n.measurements,combos_exp$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_exp_6, "simulations_exp_6.rds")
 
-set.seed(9993)
-simulations_exp_7 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_exp$dist,combos_exp$parameters,combos_exp$random_slopes,combos_exp$n.indiv,combos_exp$n.measurements,combos_exp$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_exp_7, "simulations_exp_7.rds")
+#simulation_data <- simulation_data  %>%
+ # mutate(Z_p.value= 2 *pnorm(abs(S_statistic)), lower.tail = FALSE)
+#t_p.value = 2*pt(abs(S_statistic), ddf = n.indiv * n.measurements - 3)))
 
-set.seed(3009)
-simulations_exp_8 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_exp$dist,combos_exp$parameters,combos_exp$random_slopes,combos_exp$n.indiv,combos_exp$n.measurements,combos_exp$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_exp_8, "simulations_exp_8.rds")
-
-set.seed(2203)
-simulations_exp_9 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_exp$dist,combos_exp$parameters,combos_exp$random_slopes,combos_exp$n.indiv,combos_exp$n.measurements,combos_exp$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_exp_9, "simulations_exp_9.rds")
-
-set.seed(9991)
-simulations_exp_10 <- 1:numsim %>%
-   map_df(~ pmap_dfr(list(combos_exp$dist,combos_exp$parameters,combos_exp$random_slopes,combos_exp$n.indiv,combos_exp$n.measurements,combos_exp$corr),.f = sim_mixedeffects))
-#saveRDS(simulations_exp_10, "simulations_exp_10.rds")
-
-
+# check if t and z pvalue is significant 
+# group 
--- SimulationCode/SimResults.R
+++ SimulationCode/SimResults.R
@@ -1,126 +1,91 @@
 library(tidyverse)
 library(here)
+options(scipen = 99999)
 
-######## 
-large_run_normal <- readRDS(here("SimulationData","large_run_normal.rds"))
-large_run_normal %>% filter(KR_p.value < .05) %>% nrow()
-681/36000
-# type one error rate
 
 sim_exp <- data.frame()
-for(i in 1:10){
+for(i in 1:5){
   name <- paste0("simulations_exp_",i,".rds")
   sim_exp <- rbind(sim_exp, readRDS(here("SimulationData",name)))
 }
 
 sim_lnormal <- data.frame()
-for(i in 1:10){
+for(i in 1:5){
   name <- paste0("simulations_lnormal_",i,".rds")
   sim_lnormal <- rbind(sim_lnormal, readRDS(here("SimulationData",name)))
 }
 
 sim_normal <- data.frame()
-for(i in 1:10){
+for(i in 1:5){
   name <- paste0("simulations_normal_",i,".rds")
   sim_normal <- rbind(sim_normal, readRDS(here("SimulationData",name)))
 }
 
+all_sim <- rbind(sim_exp,sim_lnormal, sim_normal) %>%
+  mutate(Z_p.value= 2*pnorm(abs(S_statistic), lower.tail = FALSE),
+         t_p.value = 2*pt(abs(S_statistic), lower.tail = FALSE,df = (number_individuals * number_measurements) - 3),
+         KR_sig = ifelse(KR_p.value < .05,TRUE,FALSE),
+         S_sig = ifelse(S_p.value < .05,TRUE,FALSE),
+         Z_sig = ifelse(Z_p.value < .05,TRUE,FALSE),
+         t_sig = ifelse(t_p.value < .05,TRUE,FALSE))
 
-# KR VS S: same
-sim_exp %>%
-  filter(KR_p.value < .05) %>% nrow()
-83/3600
-
-sim_exp %>%
-  filter(S_p.value < .05) %>% nrow()
-
-sim_lnormal %>%
-  filter(KR_p.value < .05) %>% nrow()
-117/4800
-
-sim_normal %>%
-  filter(KR_p.value < .05) %>% nrow()
-72/3600
-# Number of individuals: less individuals more error rate
-sim_exp %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(number_individuals) %>%
-  count() %>%
-  mutate(type1 = n/1200)
-
-sim_lnormal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(number_individuals) %>%
-  count() %>%
-  mutate(type1 = n/1600)
-
-sim_normal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(number_individuals) %>%
-  count() %>%
-  mutate(type1 = n/1200)
-
-# Params 
-sim_exp %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(params) %>%
-  count() %>%
-  mutate(type1 = n/1200)
-
-sim_lnormal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(params) %>%
-  count() %>%
-  mutate(type1 = n/1200)
-
-sim_normal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(params) %>%
-  count() %>%
-  mutate(type1 = n/1200)
-
-# Number of measurements 
-sim_exp %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(number_measurements) %>%
-  count() %>%
-  mutate(type1 = n/1800)
-
-sim_lnormal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(number_measurements) %>%
-  count() %>%
-  mutate(type1 = n/2400)
-
-sim_normal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(number_measurements) %>%
-  count() %>%
-  mutate(type1 = n/1800)
-
-# RSlope vs intercept only 
-sim_exp %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(rslope) %>%
-  count() %>%
-  mutate(type1 = n/1800)
-
-sim_lnormal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(rslope) %>%
-  count() %>%
-  mutate(type1 = n/2400)
-
-sim_normal %>%
-  filter(KR_p.value < .05) %>% 
-  group_by(rslope) %>%
-  count() %>%
-  mutate(type1 = n/1800)
+#saveRDS(all_sim,"all_sim.rds")
 
 
+all_sim_sum <- all_sim %>%
+  group_by(distribution,
+                    number_individuals,
+                    params,
+                    number_measurements,
+                    rslope,
+                    KR_effect, KR_term, skew, kurtosis) %>%
+  summarize(
+            KR_t1err = mean(KR_sig),
+            S_t1err = mean(S_sig),
+            Z_t1err = mean(Z_sig),
+            t_t1err = mean(t_sig))
+
+all_sim_sum %>%
+  filter(KR_term == "treatment") %>%
+  pivot_wider(names_from = c("number_individuals","rslope"),
+              values_from = "KR_t1err")%>% group_by(distribution, params,number_measurements) %>%
+  summarise_all(funs(.[!is.na(.)])) %>%
+  slice(1) %>%
+  ungroup() %>%
+  select(-c(KR_effect,KR_term,S_t1err,Z_t1err,t_t1err, distribution)) %>%
+  kbl() %>%
+  pack_rows("Exponential", 1, 4) %>%
+  pack_rows("Normal", 5, 6) %>%
+  pack_rows("Lognormal", 7, 10) %>%
+  add_header_above(c(" " = 2, "Random Intercept" = 1, "Random Slope" = 1,"Random Intercept" = 1,
+                     "Random Slope" = 1,"Random Intercept" = 1,"Random Slope" = 1)) %>%
+  add_header_above(c(" " = 2, "10" = 2, "18" = 2, "26" = 2)) %>%
+  add_header_above(c(" " = 2, "Sample Size" = 6))  
 
 
-all_sim <- rbind(sim_exp,sim_lnormal, sim_normal)
+all_sim_sum %>%
+  filter(KR_term == "treatment") %>%
+  pivot_longer(cols = c("KR_t1err","S_t1err","Z_t1err","t_t1err"),names_to = "DF_method", values_to = "error_rate") %>%
+  filter(DF_method %in% c("KR_t1err","S_t1err")) %>%
+  pivot_wider(names_from = c("rslope"),
+              values_from = "error_rate", names_prefix = "slope") %>%
+  group_by(number_individuals,skew,kurtosis,DF_method) %>%
+  summarize(random_intercept = mean(slopeFALSE), random_slope = mean(slopeTRUE)) %>%
+  pivot_wider(names_from = c("number_individuals"),
+              values_from = c("random_intercept","random_slope")) %>%
+  select(skew,kurtosis,DF_method,random_intercept_10,random_slope_10,random_intercept_18,
+         random_slope_18,random_intercept_26,random_slope_26) %>%
+  ungroup() %>%
+  kbl() %>%
+  pack_rows("Exponential", 7, 8) %>%
+  pack_rows("Normal", 1, 2) %>%
+  pack_rows("Lognormal", 3, 6) %>%
+  add_header_above(c(" " = 3, "Random Intercept" = 1, "Random Slope" = 1,"Random Intercept" = 1,
+                     "Random Slope" = 1,"Random Intercept" = 1,"Random Slope" = 1)) %>%
+  add_header_above(c(" " = 3, "10" = 2, "18" = 2, "26" = 2)) %>%
+  add_header_above(c(" " = 3, "Sample Size" = 6))  
+
+
 
 
 
